ae38ac6fbe4f490d1dd3f757c851e584e8b62a16
mylavij <159325680+mylavij@users.noreply.github.com>
2024-05-22 15:00:18 -0400

    ghj

---
+++ ApplicationRobot.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.awt.event.InputEvent;
+public class ApplicationRobot{
+
+    public static void main(String[] args) throws Exception{
+        Robot wally = new Robot();
+        wally.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+    }
+
+
+
+
+
+
+
+}
