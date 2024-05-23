c5e3c6572cc9aa65a1d96b3dd702f5b289026c5e
mylavij <159325680+mylavij@users.noreply.github.com>
2024-05-23 14:59:07 -0400

    jihk

--- ApplicationRobot.java
+++ ApplicationRobot.java
@@ -4,6 +4,10 @@ public class ApplicationRobot{
 
     public static void main(String[] args) throws Exception{
         Robot wally = new Robot();
+        wally.mouseMove(10, 1050);
+        wally.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        wally.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        wally.mouseMove(80, 880);
         wally.mousePress(InputEvent.BUTTON1_DOWN_MASK);
     }
 
